26b49d8aaa7a818f9e3157f444d5d6527b0462a4
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-08-06 01:23:31 +0100

    chore: add folder structure

---
+++ Scribit/Core/Features/Home/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Scribit
+//
+//  Created by Godwin IE on 06/08/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
