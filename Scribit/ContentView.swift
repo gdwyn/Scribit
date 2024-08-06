af65540945a2bac0d99c3d7cdd95790e5a1cc5cc
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-08-06 01:16:12 +0100

    💥 big bang

---
+++ Scribit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scribit
+//
+//  Created by Godwin IE on 05/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scribit/ScribitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribitApp.swift
+//  Scribit
+//
+//  Created by Godwin IE on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScribitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
